2be9cff9a2177bca4026e34c0aad9431a1ed3ec1
Alisson Alves <alisson2012alves@gmail.com>
2022-10-23 20:56:34 -0300

    Correção em uma parte do código

--- src/components/RootContent/rootContent.tsx
+++ src/components/RootContent/rootContent.tsx
@@ -3,7 +3,7 @@ import { RootContentMenuButton } from "./components/rootContentMenuButton";
 
 export const RootContent = () => {
   return (
-    <div className="w-screen flex flex-row md:w-[100vw] z-0">
+    <div className="flex flex-row w-full z-0">
       <RootContentMenuButton />
       <Outlet />
     </div>
